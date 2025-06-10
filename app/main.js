75296e2d9b9ff28611ef51de67f91aa39589793f
root <root@devops.lxd>
2025-06-10 13:16:16 +0000

    nigga2

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Meow Meow Nigga');
+  res.send('Meow Meow Nigga2');
 });
 
 app.listen(8000, () => {
